93f437117b04b9fec7114da3d3a25f4c486f739e
Lee Loong <31263756+gnool@users.noreply.github.com>
2017-10-02 14:29:44 +0800

    Update graph.cpp

--- graph/dijkstra/cpp/graph.cpp
+++ graph/dijkstra/cpp/graph.cpp
@@ -10,19 +10,19 @@ Graph::Graph() {
 }
 
 // Parameterized constructor.
-Graph::Graph(int total_vertices) {
+Graph::Graph(unsigned int total_vertices) {
   vertices_ = total_vertices;
   edges_ = 0;
   adjacency.resize(total_vertices);
 }
 
 // Return number of vertices.
-int Graph::vertices() {
+unsigned int Graph::vertices() const {
   return vertices_;
 }
 
 // Return number of edges.
-int Graph::edges() {
+unsigned int Graph::edges() const {
   return edges_;
 }
 
@@ -93,6 +93,28 @@ double Graph::edge(int source, int target) {
 }
 
 // Return a vector of tuples, each tuple represents one neighbour.
-vector<tuple<int,double>> Graph::neighbours(int vertex_index) {
+vector<tuple<int,double>> Graph::neighbours(int vertex_index) const {
   return adjacency[vertex_index];
+}
+
+// Print out graph by listing all edges (target, cost) of each vertice.
+ostream& operator<<(ostream& os, const Graph& graph) {
+  os << "Printing graph..." << endl;
+  os << "Graph contains " << graph.vertices() << " vertices and " << graph.edges() << " directed edges." << endl;
+  os << "Listing all vertices with their edges..." << endl;
+  os << "Vertex: (Neighbour vertex, cost)" << endl;
+  os.precision(2);
+  os.setf(ios::fixed, ios::floatfield);
+  vector<tuple<int,double>> neighbours;
+  tuple<int,double> edge;
+  for (unsigned int i = 0; i < graph.vertices(); i++) {
+    neighbours = graph.neighbours(i);
+    os << "Vertex " << i << ": ";
+    for (unsigned int j = 0; j < neighbours.size(); j++) {
+      edge = neighbours[j];
+      os << "(" << get<0>(edge) << ", " << get<1>(edge) << ") ";
+    }
+    os << endl;
+  }
+  return os;
 }
