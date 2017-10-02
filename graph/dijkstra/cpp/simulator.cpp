5955fc258532eab9d866e099e901278600ce2db9
Lee Loong <31263756+gnool@users.noreply.github.com>
2017-10-02 14:32:49 +0800

    Update simulator.cpp

--- graph/dijkstra/cpp/simulator.cpp
+++ graph/dijkstra/cpp/simulator.cpp
@@ -1,50 +1,75 @@
 #include <iostream>
 #include <limits>
 #include "simulator.h"
-#include "shortest_path.h"
 
 using namespace std;
 
-// Parameterized constructor that initializes the member variables.
-Simulator::Simulator(unsigned int size, double density, double min_distance, double max_distance) {
-  this->size_ = size;
-  this->density_ = density;
-  this->min_distance_ = min_distance;
-  this->max_distance_ = max_distance;
+// Default constructor.
+Simulator::Simulator() {
   mt_rand.seed(rand());  // seed Mersenne Twister RNG (for edge probability)
   mt_rand2.seed(rand());  // seed Mersenne Twister RNG (for edge weight)
 }
 
+// Parameterized constructor that involves building the graph directly.
+Simulator::Simulator(unsigned int size, double density, double min_distance, double max_distance) {
+  // Store the graph configuration in member variables.
+  size_ = size;
+  density_ = density;
+  min_distance_ = min_distance;
+  max_distance_ = max_distance;
+  mt_rand.seed(rand());  // seed Mersenne Twister RNG (for edge probability)
+  mt_rand2.seed(rand());  // seed Mersenne Twister RNG (for edge weight)
+  // Build the graph.
+  buildGraph(size, density, min_distance, max_distance);
+  findShortestPath();
+}
+
 // Build a random graph with given size, density, minimum and maximum edge weight.
-Graph Simulator::buildGraph() {
-  double weight;
+void Simulator::buildGraph(unsigned int size, double density, double min_distance, double max_distance) {
+  // Store the graph configuration in member variables.
+  size_ = size;
+  density_ = density;
+  min_distance_ = min_distance;
+  max_distance_ = max_distance;
   // Uniform distribution [0,1] for probability of creating an edge.
   uniform_real_distribution<> dis(0,1);
   // Uniform distribution [min,max] for the edge weight.
-  uniform_real_distribution<> dis2(min_distance_,max_distance_);
+  uniform_real_distribution<> dis2(min_distance,max_distance);
   // Initialize a graph with given size.
-  Graph graph(size_);
-  for (unsigned int i = 0; i < size_; i++) {
-    for (unsigned int j = i+1; j < size_; j++) {
+  Graph new_graph(size);
+  for (unsigned int i = 0; i < size; i++) {
+    for (unsigned int j = i+1; j < size; j++) {
       // Recall that mt_rand is for edge probability, while mt_rand2 is for
       // edge weight.
-      if (dis(mt_rand) < density_) {
-        weight = dis2(mt_rand2);  // generate random edge weights
-        graph.addEdge(i,j,weight);
-        graph.addEdge(j,i,weight);
+      if (dis(mt_rand) < density) {
+        double weight = dis2(mt_rand2);  // generate random edge weights
+        new_graph.addEdge(i,j,weight);
+        new_graph.addEdge(j,i,weight);
       }
     }
   }
-  return graph;
+  graph_ = new_graph;
+}
+
+// Find the shortest paths from source to all other vertices based on the member graph_.
+void Simulator::findShortestPath() {
+  ShortestPath new_path(graph_,0);  // set source to 0
+  new_path.findDistanceToAll();  // find shortest paths from source to all other vertices
+  path_ = new_path;
 }
 
 // Estimate the average path length by averaging over specified number of iterations.
 double Simulator::calcAveragePathLength(unsigned int iterations) {
+  // If iterations is set to zero, the simulator returns average path length of the existing member graph.
+  if (!iterations) {
+    return path_.averagePathLength();
+  }
+  // Otherwise, create different random graphs and average the average path length over the iterations.
   double iteration_sum = 0, average_path_length;
   for (unsigned int i = 0; i < iterations; i++) {
-    ShortestPath path(buildGraph(),0);  // set source to 0
-    path.findDistanceToAll();  // find shortest distance to all vertices from source
-    average_path_length = path.averagePathLength();
+    buildGraph(size_, density_, min_distance_, max_distance_);
+    findShortestPath();
+    average_path_length = path_.averagePathLength();
     if (average_path_length == numeric_limits<double>::infinity()) continue;
     iteration_sum += average_path_length;
   }
@@ -89,4 +114,14 @@ double Simulator::max_distance() {
 // Update maximum edge weight.
 void Simulator::max_distance(double new_max_distance) {
   max_distance_ = new_max_distance;
+}
+
+// Return the Graph instance currently stored.
+Graph Simulator::graph() const {
+  return graph_;
+}
+
+// Return the ShortestPath instance currently stored.
+ShortestPath Simulator::path() const {
+  return path_;
 }
