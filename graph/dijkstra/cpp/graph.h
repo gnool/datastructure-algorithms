04b59d927bb0f637de5f94f5febcc0cb296afa20
Lee Loong <31263756+gnool@users.noreply.github.com>
2017-10-02 14:30:14 +0800

    Update graph.h

--- graph/dijkstra/cpp/graph.h
+++ graph/dijkstra/cpp/graph.h
@@ -4,18 +4,17 @@
 #include <vector>
 #include <tuple>
 
-// Graph implemented using adjacency representation (i.e. edge lists).
-// All vertices are numbered 0, 1, 2, ...
+// Graph implemented using adjacency list representation (i.e. edge lists).
 class Graph {
   public:
     // Default constructor.
     Graph();
     // Parameterized constructor.
-    Graph(int total_vertices);
+    Graph(unsigned int total_vertices);
     // Return number of vertices.
-    int vertices();
+    unsigned int vertices() const;
     // Return number of edges.
-    int edges();
+    unsigned int edges() const;
     // Add a new edge between source and target with weight.
     void addEdge(int source, int target, double weight);
     // Delete an edge pointing from source to target.
@@ -27,17 +26,20 @@ class Graph {
     // Return the weight of edge from source to target.
     double edge(int source, int target);
     // Return a vector of tuples, each tuple representing one neighbour.
-    std::vector<std::tuple<int,double>> neighbours(int vertex_index);
+    std::vector<std::tuple<int,double>> neighbours(int vertex_index) const;
     
   private:
     // Total vertices.
-    int vertices_;
+    unsigned int vertices_;
     // Total edges.
-    int edges_;
+    unsigned int edges_;
     // Adjacency representation of graph.
     // Basically a 2D-vector storing tuples, where each tuple represents a neighbour
     // and contains the neighbour's vertex id as well as the edge weight between them.
     std::vector<std::vector<std::tuple<int,double>>> adjacency;
 };
+
+// Print out graph by listing all edges (target, cost) of each vertice.
+std::ostream& operator<<(std::ostream& os, const Graph& graph);
 
 #endif
