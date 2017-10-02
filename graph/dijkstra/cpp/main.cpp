130c5807b00464de962c7f456e8f0b1a41c89527
Lee Loong <31263756+gnool@users.noreply.github.com>
2017-10-02 14:30:41 +0800

    Update main.cpp

--- graph/dijkstra/cpp/main.cpp
+++ graph/dijkstra/cpp/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include <time.h>
 #include "simulator.h"
 
@@ -6,13 +7,30 @@ using namespace std;
 
 int main() {
   srand(time(0));  // for seeding the Mersenne Twister
+  filebuf fb;
+  fb.open ("dijkstra_output.txt",ios::out);
+  ostream os(&fb);
   // For density = 0.2
-  Simulator sim(5000, 0.2, 1.0, 10.0);
-  cout << "Average path length (50 nodes, 0.2 density, 1.0 min edge weight and "
-       << "10.0 max edge weight): " << sim.calcAveragePathLength(1) << endl;
+  Simulator sim(50, 0.2, 1.0, 10.0);
+  os << "Simulating graph..." << endl;
+  os << "> Total nodes:         50" << endl;
+  os << "> Density:             0.2" << endl;
+  os << "> Minimum edge weight: 1.0" << endl;
+  os << "> Maximum edge weight: 10.0" << endl << endl;
+  os << sim.graph() << endl;
+  os << sim.path() << endl;
   // For density = 0.4
-  Simulator sim2(5000, 0.4, 1.0, 10.0);
-  cout << "Average path length (50 nodes, 0.4 density, 1.0 min edge weight and "
-       << "10.0 max edge weight): " << sim2.calcAveragePathLength(1) << endl;
+  Simulator sim2(50, 0.4, 1.0, 10.0);
+  os << "Simulating graph..." << endl;
+  os << "> Total nodes:         50" << endl;
+  os << "> Density:             0.4" << endl;
+  os << "> Minimum edge weight: 1.0" << endl;
+  os << "> Maximum edge weight: 10.0" << endl << endl;
+  os << sim2.graph() << endl;
+  os << sim2.path() << endl;
+  os << "Gathering statistics..." << endl;
+  os << "Average path length (density=0.2) over 10000 random graphs: " << sim.calcAveragePathLength(10000) << endl;
+  os << "Average path length (density=0.4) over 10000 random graphs: " << sim2.calcAveragePathLength(10000) << endl;
+  fb.close();
   return 0;
 }
